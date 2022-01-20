2e544b8a442cd1457802a87eef4bc6dabc7c22e0
yashoka9876 <98081691+yashoka9876@users.noreply.github.com>
2022-01-20 15:33:57 +0530

    Add files via upload
    this the program to check out the name of the month of year by taking input through the user

---
+++ trainingJK4.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    int mon_no;
+    printf("enter the month number :");
+    scanf("%d",&mon_no);
+    switch(mon_no){
+    case 1:printf("january");
+    break;
+    case 2:printf("february");
+    break;
+    case 3:printf("march");
+    break;
+    case 4:printf("april");
+    break;
+    case 5:printf("may");
+    break;
+    case 6:printf("june");
+    break;
+    case 7:printf("july");
+    break;
+    case 8:printf("August");
+    break;
+    case 9:printf("september");
+    break;
+    case 10:printf("october");
+    break;
+    case 11:printf("November");
+    break;
+    case 12:printf("December");
+    break;
+
+    }
+}
